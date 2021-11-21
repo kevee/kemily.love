30ea9d70cd0b1188ab3531dd55b9a03cfc87a0fc
Kevin Miller <keveemiller@gmail.com>
2021-11-21 09:18:00 -0800

    chore: Fix hydration on photo page

--- src/pages/photos/index.js
+++ src/pages/photos/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useStaticQuery, graphql, Link } from 'gatsby'
+import React, { useEffect, useState } from 'react'
+import { graphql, Link } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Masonry from 'react-masonry-css'
 import styled from '@emotion/styled'
@@ -20,9 +20,6 @@ const ImageMasonry = styled(Masonry)`
     padding-left: 1rem; /* gutter size */
     background-clip: padding-box;
   }
-
-  & > div {
-  }
 `
 
 const ClickableImage = styled.div`
@@ -35,113 +32,104 @@ const ClickableImage = styled.div`
   }
 `
 
-const LargeImage = styled.img`
-  margin-top: 0.5rem;
-  margin-bottom: 0;
-`
-
 const toTitleCase = (str) =>
   str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
   })
 
-const PhotosPage = () => {
-  const [selected, setSelected] = useState()
-  const data = useStaticQuery(graphql`
-    {
-      allS3ImageAsset {
-        nodes {
-          childImageSharp {
-            gatsbyImageData(
-              width: 900
-              placeholder: BLURRED
-              formats: [AUTO, WEBP, AVIF]
-            )
-          }
-          Key
-        }
-      }
-      allAirtable(filter: { table: { in: ["RSVP", "Website photos"] } }) {
-        nodes {
-          data {
-            Photos {
-              localFiles {
-                childImageSharp {
-                  gatsbyImageData(
-                    width: 900
-                    placeholder: BLURRED
-                    formats: [AUTO, WEBP, AVIF]
-                  )
-                }
-              }
-            }
-          }
-          table
-        }
-      }
-    }
-  `)
-  let photos = []
-  data.allAirtable.nodes.forEach(({ data }) => {
-    if (typeof data.Photos !== 'undefined' && data.Photos) {
-      photos = [...photos, ...data.Photos.localFiles]
-    }
-  })
+const PhotosPage = ({ data }) => {
+  const [selected, setSelected] = useState(false)
+  const [hasMounted, setHasMounted] = useState(false)
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  if (!hasMounted) {
+    return null
+  }
 
   return (
     <Layout title="Photos">
-      {selected && (
-        <ModalWrapper onClose={() => setSelected(false)}>
-          <ModalContainer wide>
-            <CloseButton
-              onClick={() => {
-                setSelected(false)
+      {hasMounted && (
+        <>
+          {selected && (
+            <ModalWrapper onClose={() => setSelected(false)}>
+              <ModalContainer wide>
+                <CloseButton
+                  onClick={() => {
+                    setSelected(false)
+                  }}
+                >
+                  Close
+                </CloseButton>
+                <GatsbyImage
+                  sty
+                  image={getImage(selected)}
+                  alt=""
+                  aria-hidden
+                />
+                <p>
+                  By {toTitleCase(selected.Key.split('/')[1].replace('-', ' '))}
+                  .{' '}
+                  <a href={`/download-photo/${selected.Key}`} download>
+                    Download image
+                  </a>
+                </p>
+              </ModalContainer>
+            </ModalWrapper>
+          )}
+          <Container>
+            <h1>Shared photos from Love Week</h1>
+            <p>
+              Below are all the wonderful pictures that people took throughout
+              The Love Week.
+              <br />
+              Click on any image to download. <br />
+              <Link to="/share">Share your own photos</Link>
+            </p>
+            <ImageMasonry
+              breakpointCols={{
+                default: 3,
+                1100: 2,
+                768: 1,
               }}
             >
-              Close
-            </CloseButton>
-            <GatsbyImage sty image={getImage(selected)} alt="" aria-hidden />
-            <p>
-              By {toTitleCase(selected.Key.split('/')[1].replace('-', ' '))}.{' '}
-              <a href={`/download-photo/${selected.Key}`} download>
-                Download image
-              </a>
-            </p>
-          </ModalContainer>
-        </ModalWrapper>
+              {data.allS3ImageAsset.nodes.map((photo) => (
+                <ClickableImage>
+                  <GatsbyImage
+                    image={getImage(photo)}
+                    alt=""
+                    aria-hidden
+                    onClick={() => {
+                      setSelected(photo)
+                    }}
+                  />
+                </ClickableImage>
+              ))}
+            </ImageMasonry>
+          </Container>
+        </>
       )}
-      <Container>
-        <h1>Shared photos from Love Week</h1>
-        <p>
-          Below are all the wonderful pictures that people took throughout The
-          Love Week.
-          <br />
-          Click on any image to download. <br />
-          <Link to="/share">Share your own photos</Link>
-        </p>
-        <ImageMasonry
-          breakpointCols={{
-            default: 3,
-            1100: 2,
-            768: 1,
-          }}
-        >
-          {data.allS3ImageAsset.nodes.map((photo) => (
-            <ClickableImage>
-              <GatsbyImage
-                image={getImage(photo)}
-                alt=""
-                aria-hidden
-                onClick={() => {
-                  setSelected(photo)
-                }}
-              />
-            </ClickableImage>
-          ))}
-        </ImageMasonry>
-      </Container>
     </Layout>
   )
 }
 
 export default PhotosPage
+
+export const query = graphql`
+  {
+    allS3ImageAsset {
+      nodes {
+        childImageSharp {
+          gatsbyImageData(
+            width: 900
+            placeholder: DOMINANT_COLOR
+            formats: [AUTO, WEBP, AVIF]
+          )
+        }
+        Key
+      }
+    }
+  }
+`
